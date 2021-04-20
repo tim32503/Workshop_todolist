9db2aca8a76154062a94afce5b05f451d2a50636
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-04-20 13:52:59 +0800

    修改刪除功能

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
+  
   // 1 - 標記完成功能
   document.querySelectorAll('li').forEach(function(item) {
     item.addEventListener('click', function(e){
@@ -13,20 +14,7 @@ document.addEventListener('DOMContentLoaded', function(){
     });
   });
 
-  document.getElementById('addBtn').addEventListener('click', function(e){
-    let li = document.createElement('li');
-    let span = document.createElement('span');
-    let text = e.target.previousElementSibling;
-
-    span.textContent = "x";
-    span.classList.add('close');
-
-    li.textContent = text.value;
-    text.value = "";
-
-    li.appendChild(span); 
-    document.querySelector('ul').appendChild(li);
-  });
+  
 
   
 });
