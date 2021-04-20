156721e99eadd7e3151fa15c916f308c1d9cf192
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-04-20 13:54:17 +0800

    修改新增功能

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
+  // 3 - 新增功能
   document.getElementById('addBtn').addEventListener('click', function(e){
     let li = document.createElement('li');
     let span = document.createElement('span');
@@ -6,9 +7,15 @@ document.addEventListener('DOMContentLoaded', function(){
 
     span.textContent = "x";
     span.classList.add('close');
+    span.addEventListener('click', function(spanEl){
+      spanEl.target.parentElement.remove();
+    });
 
     li.textContent = text.value;
     text.value = "";
+    li.addEventListener('click', function(liEl){
+      liEl.target.classList.toggle('checked');
+    });
 
     li.appendChild(span); 
     document.querySelector('ul').appendChild(li);
