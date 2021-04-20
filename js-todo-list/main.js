1a68e138994e0de3f221c15dea48f2f42bad56a4
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-04-20 13:56:23 +0800

    修改刪除功能

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,11 +1,4 @@
 document.addEventListener('DOMContentLoaded', function(){
-  
-  // 1 - 標記完成功能
-  document.querySelectorAll('li').forEach(function(item) {
-    item.addEventListener('click', function(e){
-      e.target.classList.toggle('checked');
-    });
-  });
 
   // 2 - 刪除功能
   document.querySelectorAll('.close').forEach(function(item){
@@ -14,7 +7,4 @@ document.addEventListener('DOMContentLoaded', function(){
     });
   });
 
-  
-
-  
 });
