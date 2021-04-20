e5cb2368a7b3c6cebdb92f99eba0e1e60c5bcfed
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-04-20 13:57:25 +0800

    修改標記完成功能

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,22 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
-  document.getElementById('addBtn').addEventListener('click', function(e){
-    let li = document.createElement('li');
-    let span = document.createElement('span');
-    let text = e.target.previousElementSibling;
-
-    span.textContent = "x";
-    span.classList.add('close');
-
-    li.textContent = text.value;
-    text.value = "";
-
-    li.appendChild(span); 
-    document.querySelector('ul').appendChild(li);
-  });
-
+  // 1 - 標記完成功能
   document.querySelectorAll('li').forEach(function(item) {
     item.addEventListener('click', function(e){
       e.target.classList.toggle('checked');
     });
   });
+  
 });
