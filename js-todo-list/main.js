4d59adfd110b24a1e4ee684ea1ec5cc607a98067
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-04-20 12:00:16 +0800

    完成按鈕新增功能

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,16 @@
-// TO DO
+document.addEventListener('DOMContentLoaded', function(){
+  document.getElementById('addBtn').addEventListener('click', function(e){
+    let li = document.createElement('li');
+    let span = document.createElement('span');
+    let text = e.target.previousElementSibling;
+
+    span.textContent = "x";
+    span.classList.add('close');
+
+    li.textContent = text.value;
+    text.value = "";
+
+    li.appendChild(span); 
+    document.querySelector('ul').appendChild(li);
+  });
+});
